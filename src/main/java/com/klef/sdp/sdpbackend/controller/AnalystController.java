dd81303fc0d7150223edbf1cbce5adb59eff7b4a
venkateswarakunduruai <venkateswarakunduru.ai@gmail.com>
2026-04-08 10:17:18 +0530

    Update AnalystController.java

--- src/main/java/com/klef/sdp/sdpbackend/controller/AnalystController.java
+++ src/main/java/com/klef/sdp/sdpbackend/controller/AnalystController.java
@@ -1,49 +1,30 @@
-package com.klef.sdp.sdpbackend.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.klef.sdp.sdpbackend.entity.Analyst;
-import com.klef.sdp.sdpbackend.service.AnalystService;
-
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/analyst")
 @CrossOrigin("*")
 public class AnalystController {
 
-	@Autowired
-	private AnalystService analystservice;
-	
-	@GetMapping("/")
-	 public String analysthome()
-	  {
-		   return "Analyst Controller Demo";
-	  }
-	
-	@PostMapping("/login")
-	 public ResponseEntity<?> verifyAnalystlogin(@RequestBody Analyst analyst)
-	  {
-		   try
-			{
-				Analyst a = analystservice.verifyAnalystLogin(analyst.getEmail(), analyst.getPassword());
-			
-			    if(a!=null)
-			    {
-			    	return ResponseEntity.status(200).body(a);
-			    }
-			    else
-			    {
-			    	return ResponseEntity.status(401).body("Login Invalid");
-			    }
-			}
-			catch (Exception e) 
-			{
-				return ResponseEntity.status(500).body("Internal Server Error");
-			}
-	  }
+    @Autowired
+    private AnalystService analystService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Analyst analyst) {
+        Analyst a = analystService.verifyAnalystLogin(analyst.getEmail(), analyst.getPassword());
+        if (a != null) return ResponseEntity.ok(a);
+        else return ResponseEntity.status(401).body("Login Invalid");
+    }
+
+    @GetMapping("/viewallissues")
+    public ResponseEntity<List<Issue>> viewAllIssues() {
+        return ResponseEntity.ok(analystService.viewAllIssues());
+    }
+
+    @GetMapping("/viewallproblems")
+    public ResponseEntity<List<Issue>> viewAllProblems() {
+        return ResponseEntity.ok(analystService.viewAllProblems());
+    }
+
+    @PostMapping("/solveissue/{id}")
+    public ResponseEntity<Issue> solveIssue(@PathVariable Long id) {
+        return ResponseEntity.ok(analystService.solveIssue(id));
+    }
 }
