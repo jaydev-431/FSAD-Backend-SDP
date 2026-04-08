1be5c7d706d7eacc74e8fce5c947cfca2e43a268
venkateswarakunduruai <venkateswarakunduru.ai@gmail.com>
2026-04-08 10:14:22 +0530

    Update AnalystService.java

--- src/main/java/com/klef/sdp/sdpbackend/service/AnalystService.java
+++ src/main/java/com/klef/sdp/sdpbackend/service/AnalystService.java
@@ -1,8 +1,14 @@
 package com.klef.sdp.sdpbackend.service;
 
 import com.klef.sdp.sdpbackend.entity.Analyst;
+import com.klef.sdp.sdpbackend.entity.Issue;
+import java.util.List;
 
-public interface AnalystService 
-{
-	public Analyst verifyAnalystLogin(String email,String pwd);
+public interface AnalystService {
+    Analyst verifyAnalystLogin(String email, String pwd);
+    
+    // New methods for dashboard
+    List<Issue> viewAllIssues();
+    List<Issue> viewAllProblems();
+    Issue solveIssue(Long id);
 }
